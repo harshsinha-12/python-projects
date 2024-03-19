1bdcde580600846052e4294a5f263eeaa791ee7c
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-19 17:13:56 +0530

    cases

--- Youtube Manager/youtube_manager.py
+++ Youtube Manager/youtube_manager.py
@@ -1,5 +1,21 @@
 
 
+
+
+def list_all_videos(videos):
+    pass
+
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+videos = []
+
 while True:
     print("\n Youtube Manager | Choose an option:")
     print("1. List a Youtube video")  
@@ -10,6 +26,15 @@ while True:
     choice = input("Enter your choice: ")
     match choice:
         case "1":
-            list_all_videos(video)
-        
+            list_all_videos(videos)
+        case "2":
+            add_video(videos)
+        case "3":
+            update_video(videos)
+        case "4":   
+            delete_video(videos)
+        case "5":
+            break
+        case _:
+            print("Invalid choice. Please try again")
 
